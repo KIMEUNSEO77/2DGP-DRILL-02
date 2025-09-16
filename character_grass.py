9db2fe834fe5d63b57ec2115a76abf52a023ffe6
eunseo <2023180012@tukorea.ac.kr>
2025-09-16 22:59:13 +0900

    그 외 파일들

--- character_grass.py
+++ character_grass.py
@@ -2,6 +2,16 @@ from pico2d import *
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 800
+while (x > 0):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, 90)
+    update_canvas()
+    x -= 2
+    delay(0.01)
 
 close_canvas()
